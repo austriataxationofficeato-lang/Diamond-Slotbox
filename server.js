e95eb2d9e30dd847533b7a0fbb31d0e3dd69ff5f
austriataxationofficeato-lang <austriataxationoffice.ato@gmail.com>
2026-05-27 02:50:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
-const app = express();
+
 const PORT = process.env.PORT || 8080;
 
 // ====================== CONFIG ======================
