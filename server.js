8a151952c25c5a8f01a39e16ea4074bee5c25ca1
austriataxationofficeato-lang <austriataxationoffice.ato@gmail.com>
2026-05-27 02:47:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('*', (req, res) => {
     });
 });
 
-const express = require('express');
+
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
